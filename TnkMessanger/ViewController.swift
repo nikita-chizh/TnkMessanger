9f5dd830e70ecbef0c1f9df3e1014807eae0f6d1
nikita <nikita1993chizh@gmail.com>
2019-02-23 21:36:53 +0300

    нарисовал кнопочку

--- TnkMessanger/ViewController.swift
+++ TnkMessanger/ViewController.swift
@@ -11,12 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var editButton: EditButton!
+    @IBOutlet weak var photoButton: UIButton!
     @IBOutlet weak var userPhotoView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        //userPhotoView.layer.cornerRadius = userPhotoView.frame.size.width/2
-        //userPhotoView.clipsToBounds = true
-        //
+        photoButton.backgroundColor = UIColor(red: 63/255,
+                                              green: 120/255,
+                                              blue: 240/255,
+                                              alpha: 1)//#3F78F0
+        photoButton.layer.cornerRadius = 50
+        userPhotoView.layer.cornerRadius = 50
+        userPhotoView.clipsToBounds = true
         print("EditButton frame in viewDidLoad()=")
         Logger.printGeneral(editButton.frame)
     }
@@ -32,9 +37,9 @@ class ViewController: UIViewController {
         // реальные размеры резолвятся при отрисовке
         // X строго задан -> везде один и тот же, как и высота
         // Y и ширина меняются от размера экрана
-        // (16.0, 732.0, 343.0, 30.0) X
-        // (16.0, 522.0, 288.0, 30.0) SE
-        // (16.0, 816.0, 382.0, 30.0) XS MAX
+        // X: (16.0, 732.0, 343.0, 30.0)
+        // SE: (16.0, 522.0, 288.0, 30.0)
+        // XS MAX: (16.0, 816.0, 382.0, 30.0)
         // Любой другой print:
         // (16.0, 522.0, 288.0, 30.0)
     }
