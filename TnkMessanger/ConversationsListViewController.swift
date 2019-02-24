ac9224fba89c69374f5448bd3e0678b2d799095b
nikita <nikita1993chizh@gmail.com>
2019-02-24 16:31:37 +0300

    наверстал ячейку

---
+++ TnkMessanger/ConversationsListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConversationsListViewController.swift
+//  TnkMessanger
+//
+//  Created by Никита on 24/02/2019.
+//  Copyright © 2019 Никита. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
